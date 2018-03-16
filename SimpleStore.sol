af7c9f846fb2958f0a7c7a97f7ab637d14784b73
Richard Moore <github@ricmoo.com>
2018-03-16 17:11:14 -0400

    Added contract.

---
+++ SimpleStore.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.20;
+
+contract SimpleStore {
+    event ValueChanged(address indexed author, string oldValue, string newValue);
+
+    string _value;
+
+    function setValue(string value) public {
+        ValueChanged(msg.sender, _value, value);
+        _value = value;
+    }
+
+    function value() public constant returns (string) {
+        return _value;
+    }
+}
